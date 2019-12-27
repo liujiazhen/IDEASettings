5b55540d92349a05905621ec8043b333c0e014c5
liujiazhen <liujiazhen@live.cn>
2019-12-27 16:35:21 +0800

    IU-2019.3.1 <liujiazhen@01B2472A0027604 Create baseRefactoring.xml, markdown.xml, p3c.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, projectView.xml, javaeeExternalResources.xml, colors.scheme.xml, My Customized For Java Doc By LiuJiazhen.xml, databaseDrivers.xml, Xml.xml, mavenVersion.xml, templates.xml, Default.xml, laf.xml, docker.xml, jdk.table.xml, path.macros.xml, JiazhenLiu.xml, intentionSettings.xml, web-browsers.xml, wsl.distributions.xml, vcs.xml, toStringTemplates.xml, mybatis.xml, editor.xml, usageView.xml, debugger.xml, setterTemplates.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright © ${YEAR} LiuJiazhen
+ * 
+ * description: 
+ * @version v1.0.0
+ * @author LiuJiazhen
+ * @date ${DATE} ${TIME}
+ *
+ * Modification History
+ * Date     Author      Version     Description
+ *---------------------------------------------------------*
+ * ${DATE}      LJZ     v1.0.0      create
+ */
